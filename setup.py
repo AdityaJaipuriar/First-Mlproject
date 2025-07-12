2330861da3e9741c45710f582e997ecbb46ad9b2
Aditya Jaipuriar <adityajaipuriar30@gmail.com>
2025-07-12 14:19:13 +0530

    setups

---
+++ setup.py
@@ -0,0 +1,24 @@
+#It turns the ML project into a Python package, so others (or future me) can install it with pip install and use it like any other library.
+from setuptools import find_packages,setup
+from typing import List
+
+minus_e_dot = "-e ."
+
+def get_requirements(file_path:str)-> List[str]:
+    '''This function will return a list of packages needed to downloaded'''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+        if(minus_e_dot in requirements):
+            requirements.remove(minus_e_dot)
+
+    return requirements
+setup(
+    name='First-MLproject',
+    version = '0.0.1',
+    author='Aditya Jaipuriar',
+    author_email = 'adityajaipuriar30@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
